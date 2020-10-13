5a63d15d33ff4f568ac065bd9593f87788d80eed
Zachary <1734015+nopslip@users.noreply.github.com>
2020-10-13 12:09:14 -0600

    imporving key stuff 2

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os
 import hmac
 import hashlib
 import binascii 
+import json
+
 from flask import Flask
 from flask import request
 
@@ -13,22 +15,30 @@ def hello_world():
 
 @gtc_sig_app.route('/get_signature', methods=['POST'])
 def get_signature():
+
+    # todo - what if key doesn't exist?
+    GTC_SIG_KEY = os.environ.get('GTC_SIG_KEY')
+    
     headers = request.headers
-    print(headers)
+    gtc_sig_app.logger.info(f'HEADERS:{headers}')
     
     dict = request.form
+    gtc_sig_app.logger.info(f'POST BODY DATA:{dict}')
+
     for key in dict:
         print(f'form key - {key}')
         print(f'key value - {dict[key]}')
 
     print(f'GTC_SIG_KEY: {os.environ.get("GTC_SIG_KEY")}')
     
-    '''
-    try:
-        GTC_SIG_KEY = os.environ.get('GTC_SIG_KEY')
-        if headers['X-GITCOIN-SIG'] == 
-     except:
-    '''
+    computed_hash = create_sha256_signature(GTC_SIG_KEY, json.dumps(dict))
+    
+    gtc_sig_app.logger.info(f'COMPUTED HASH: {computed_hash}')
+
+    if headers['X-GITCOIN-SIG'] == computed_hash:
+        gtc_sig_app.logger.info('HASH MATCH!')
+    else: 
+        gtc_sig_app.logger.info('HASH NO MATCH!!')
 
     # we must respond to the request  
     return "THIS WILL BE A SIGNED MESSAGE!"
@@ -37,9 +47,13 @@ def create_sha256_signature(key, message):
     '''
     Given key & message, returns HMAC digest of the message 
     '''
-    byte_key = binascii.unhexlify(key)
-    message = message.encode()
-    return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
+    try:
+        byte_key = binascii.unhexlify(key)
+        message = message.encode()
+        return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
+    except Exception as e:
+        gtc_sig_app.logger.error(f'GTC Distributor - Error Hashing Message: {e}')
+        return False 
 
 
 if __name__ == '__main__':
