d3c91f612496038ee69020ffa587d07d06306b19
Zachary <1734015+nopslip@users.noreply.github.com>
2020-10-12 19:54:27 -0600

    typo

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ def hello_world():
     return 'Welcome!'
 
 @gtc_sig_app.route('/get_signature', methods=['POST'])
-def get_signature():
+def get_signature(request):
     headers = request.headers()
     data = request.get_data()
     print(headers)
     print(data)
-    
+
 
 if __name__ == '__main__':
     gtc_sig_app.run()
