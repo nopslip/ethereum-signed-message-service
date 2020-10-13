7e78f52b4f2a32e4d2646983397b32b944e2dd96
Zachary <1734015+nopslip@users.noreply.github.com>
2020-10-13 08:27:10 -0600

    improved key stuff

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import os
+import hmac
+import hashlib
+import binascii 
 from flask import Flask
 from flask import request
 
@@ -5,7 +9,7 @@ gtc_sig_app = Flask(__name__)
 
 @gtc_sig_app.route('/')
 def hello_world():
-    return 'Welcome!'
+    return '--GTC TOKEN SIGNER V1--'
 
 @gtc_sig_app.route('/get_signature', methods=['POST'])
 def get_signature():
@@ -14,9 +18,28 @@ def get_signature():
     
     dict = request.form
     for key in dict:
-        print(f'form key {dict[key]}')
+        print(f'form key - {key}')
+        print(f'key value - {dict[key]}')
+
+    print(f'GTC_SIG_KEY: {os.environ.get('GTC_SIG_KEY')}')
     
-    return "yah!"
+    '''
+    try:
+        GTC_SIG_KEY = os.environ.get('GTC_SIG_KEY')
+        if headers['X-GITCOIN-SIG'] == 
+     except:
+    '''
+
+    # we must respond to the request  
+    return "THIS WILL BE A SIGNED MESSAGE!"
+
+def create_sha256_signature(key, message):
+    '''
+    Given key & message, returns HMAC digest of the message 
+    '''
+    byte_key = binascii.unhexlify(key)
+    message = message.encode()
+    return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
 
 
 if __name__ == '__main__':
