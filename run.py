6c1644d9f7f39570ed129f3e77d5b9460b4b249a
Zachary <1734015+nopslip@users.noreply.github.com>
2020-12-23 13:14:16 -0700

    new rinkeby contract

--- run.py
+++ run.py
@@ -209,7 +209,7 @@ def createSignableStruct(user_id, user_address, user_amount, delegate_address):
         name='WOLF', 
         version='1.0.1', 
         chainId=4, 
-        verifyingContract='0xE671f8729e4cc7c312675C5cB655DC3647Faa059')  
+        verifyingContract='0xa8FF6B461123f896F667057f4d3E3DE6362E7ccb')  
 
     # Define our struct type
     class Claim(EIP712Struct):
